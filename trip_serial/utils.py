8cb1c9c0cb6fe634d4f3a3e14b00a91ad6b00d21
Davide Dorigoni <ddorigoni@fbk.eu>
2024-03-19 12:24:00 +0100

    add small utils functions

---
+++ trip_serial/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def saturate(val, max_val):
+    if val >= 0.0:
+        return min(val, max_val)
+    else:
+        return max(val, -max_val)
+
+def rad2deg(value_rad):
+    return value_rad * 180.0 / np.pi
+
+def deg2rad(value_deg):
+    return value_deg / 180.0 * np.pi
+
+def rpm2radps(value_rpm):
+    return value_rpm * np.pi / 30.0
+
+def radps2rpm(value_radps):
+    return value_radps * 30.0 / np.pi
