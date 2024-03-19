6aaf7577537cdebb9a53de1cc95a7fc2530d57f6
Davide Dorigoni <ddorigoni@fbk.eu>
2024-03-19 23:43:32 +0100

    add parsing and function to get angle

--- trip_serial/encoder.py
+++ trip_serial/encoder.py
@@ -1,10 +1,11 @@
+import numpy as np
 
 class Encoder:
     def __init__(self, id, ppr, name):
         self.id = id
         self.ppr = ppr
         self.vel = 0.0
-        self.pulses_count = 0
+        self.pulses_count = 0.0
         self.name = name
 
     def getPulsesCount(self):
@@ -27,12 +28,16 @@ class Encoder:
 
     def setPulsesCount(self, pulses_count):
         self.pulses_count = pulses_count
+    
+    def getAngleRAD(self):
+        return 2 * np.pi * self.pulses_count / self.ppr
 
     def setData(self, token):
+        data = self.__extractData(token)
         if(self.__isVelocityData(token)):
-            self.vel = self.__parseEncoderVelocity(token)
+            self.vel = data
         elif(self.__isPulseData(token)):
-            self.pulses_count = self.__parseEncoderPulse(token)
+            self.pulses_count = data
     
     def __isVelocityData(self, token):
         return token[2] == 'V'
@@ -40,8 +45,9 @@ class Encoder:
     def __isPulseData(self, token):
         return token[2] == 'P'
     
-    def __parseEncoderVelocity(self, data_str):
-        pass # TODO
-
-    def __parseEncoderPulse(self, data_str):
-        pass # TODO
+    def __extractData(self, token):
+        # remove encoder identifier, id, data identifier
+        sub_token = token[3:]
+        # remove noise char if present
+        sub_token = sub_token.replace(',','')
+        return float(sub_token)
