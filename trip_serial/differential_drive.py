3b8b8f7ccbf514fbf3e58edfa80612514476d20c
Davide Dorigoni <ddorigoni@fbk.eu>
2024-03-19 23:42:42 +0100

    change names

--- trip_serial/differential_drive.py
+++ trip_serial/differential_drive.py
@@ -10,17 +10,17 @@ class DifferentialDrive:
         self.right_motor_omega_radps = 0.0
         self.left_motor_omega_radps = 0.0
 
-    def getMotorsSpeed(self):
+    def getMotorsVel(self):
         return [self.left_motor_omega_radps, self.right_motor_omega_radps]
     
-    def getWheelsSpeed(self):
+    def getWheelsVel(self):
         tau = self.gearbox_ratio
         return [self.left_motor_omega_radps / tau, self.right_motor_omega_radps / tau]
     
     # use the kinematic model of the differential drive model
     # to convert velocities of the motor into velocities of 
     # the vehicle
-    def getVehicleSpeed(self):
+    def getVehicleVel(self):
         tau = self.gearbox_ratio
         r =  self.wheel_radius
         omega_r = self.right_motor_omega_radps
@@ -31,11 +31,11 @@ class DifferentialDrive:
         ang_vel = (omega_r - omega_l) * r / (tau * d)
         return [lin_vel, ang_vel]
 
-    def setMotorsSpeed(self, left_motor_omega_radps, right_motor_omega_radps):
+    def setMotorsVel(self, left_motor_omega_radps, right_motor_omega_radps):
         self.left_motor_omega_radps = left_motor_omega_radps
         self.right_motor_omega_radps = right_motor_omega_radps
 
-    def setWheelsSpeed(self, left_wheel_omega_radps, right_wheel_omega_radps):
+    def setWheelsVel(self, left_wheel_omega_radps, right_wheel_omega_radps):
         tau = self.gearbox_ratio
 
         self.left_motor_omega_radps = left_wheel_omega_radps * tau
@@ -44,11 +44,11 @@ class DifferentialDrive:
     # use the kinematic model of the differential drive model
     # to convert velocities of the vehicle into velocities of 
     # the motors
-    def setVehicleSpeed(self, lin_vel_mps, ang_vel_radps):
+    def setVehicleVel(self, lin_vel_mps, ang_vel_radps):
         r =  self.wheel_radius
         d = self.wheel_distance
 
         left_wheel_omega_radps = (lin_vel_mps - d * 0.5 * ang_vel_radps) / r
         right_wheel_omega_radps = (lin_vel_mps + d * 0.5 * ang_vel_radps) / r
 
-        self.setWheelsSpeed(left_wheel_omega_radps, right_wheel_omega_radps)
+        self.setWheelsVel(left_wheel_omega_radps, right_wheel_omega_radps)
